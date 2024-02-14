bb4f81bf9744ba81f6c9f1294479d34c5904a639
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-02-14 21:25:24 +0530

    Updated user routes

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,6 +3,7 @@ import zod from 'zod';
 import * as jwt  from 'jsonwebtoken';
 import prisma from '../index';
 import JWT_PASS from '../config';
+import bcrypt from 'bcrypt';
 
 const router: Router = express.Router();
 
@@ -21,11 +22,14 @@ router.post("/signup", async (req,res) => {
     const {username, password, firstName, lastName} = req.body;
     console.log(username, password, firstName, lastName);
 
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     try {
         const res = await prisma.user.create({
             data: {
                 username: username,
-                password: password,
+                password: hashedPassword,
                 firstName: firstName,
                 lastName: lastName,
                 account: {
@@ -49,8 +53,43 @@ router.post("/signup", async (req,res) => {
         console.log(error);
         return
     }
+});
 
 
+const signinSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+});
+router.post("/signin", async (req, res) => {
+    const parsed = signinSchema.safeParse(req.body);
+    if(!parsed.success){
+        return res.json({message:"Invalid credentials"});
+    }
+
+    const {username, password} = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    });
+
+    if(!user){
+        return res.status(411).json({message:"User doesn't exist"});
+    };
+
+    const isvalidPassword = await bcrypt.compare(password, user.password);
+    if(!isvalidPassword){
+        return res.status(411).json({message:"Invalid password"})
+    };
+
+    const token = await jwt.sign(username, JWT_PASS);
+    res.status(200).json({token: "token"});
+});
+
+
+router.get("/bulk", async (req, res) => {
+    
 })
 
 export default router;
