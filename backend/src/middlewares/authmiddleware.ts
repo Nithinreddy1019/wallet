3f285962c02dfafd76b97db33394bd803034644f
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-02-15 12:36:19 +0530

    Added middlewares

---
+++ backend/src/middlewares/authmiddleware.ts
@@ -0,0 +1,32 @@
+import JWT_PASS from "../config";
+import * as jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+
+export interface requstWithUsername extends Request {
+    username?: string;
+}
+
+interface jwtPayload {
+    username: string
+}
+
+const authMiddleware = async (req: requstWithUsername, res: Response, next: NextFunction) => {
+    const tokenInput = req.headers.authorization;
+    if(!tokenInput || !tokenInput.startsWith("Bearer ")){
+        return res.status(403).json({message:"Unauthorized"})
+    };
+
+    const token = tokenInput?.split(" ")[1];
+    try {
+        const authenticated = jwt.verify(token, JWT_PASS) as jwtPayload;
+        req.username = authenticated.username;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(403).json({message: "An error occured"})
+    }
+
+}
+
+export default authMiddleware;
