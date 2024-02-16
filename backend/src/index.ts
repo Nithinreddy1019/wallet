9302450b63e2a54eb9b7c44a9c79d7d51b1d35e7
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-02-16 13:04:39 +0530

    Updated index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,9 @@ import cors from 'cors';
 import mainRouter from "./routes/index";
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
-
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.DATABASE_URL);
+
 
 const app = express();
 app.use(cors());
